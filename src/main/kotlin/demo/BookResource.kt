0f378b83168696e86351f5757475270ac2a6174c
Junwei Dai <junwei.dai@JunweideMacBook-Pro.local>
2021-07-14 11:23:06 +0800

    [junwei] change time from String to LocalDateTime

--- src/main/kotlin/demo/BookResource.kt
+++ src/main/kotlin/demo/BookResource.kt
@@ -2,13 +2,14 @@ package demo
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 import java.util.*
 
 @RestController
 class BookResource {
     @GetMapping(value= ["/books"])
     fun books(): List<Books> = listOf(
-        Books(23.toString(),"book","jane","12:00")
+        Books(23.toString(),"book","jane", LocalDateTime.now())
     )
 }
-data class Books(val id: String?, val name: String, val author:String, val writeAt: String)
+data class Books(val id: String?, val name: String, val author:String, val writeAt:LocalDateTime )
